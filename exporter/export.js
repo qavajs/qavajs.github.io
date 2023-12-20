336e2f01851f1c81c52e7271fdb160ff01e66b0b
Alexander Galichenko <alexandr.galichenko@gmail.com>
2023-12-20 17:51:24 +0200

    added docs for all and any of memory steps (#67)
    * added docs for all and any of memory steps

--- exporter/export.js
+++ exporter/export.js
@@ -1,7 +1,7 @@
 const glob = require('glob');
 const { readFileSync, writeFileSync } = require('node:fs');
 
-const mds = glob.sync('./docs/**/*.md');
+const mds = glob.sync('./docs/**/*.{md,mdx}');
 
 writeFileSync('./fullDoc.txt', mds.map(file => readFileSync(file, 'utf-8')).join('\n'));
 
